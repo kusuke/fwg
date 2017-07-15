2c8cbfc88b15fb835d7bc88d1aac24216c54ac50
muranaka <r.muranaka@leihauoli.com>
2017-07-15 23:04:31 +0900

    add reset scss

--- app/components/templates/Root/Root.jsx
+++ app/components/templates/Root/Root.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 
+import classNames from 'classnames/bind';
+import styles from '../../../common/scss/common';
+const css = classNames.bind(styles);
+
 export default function render(props) {
   const { children } = props;
   return (
-    <div>
+    <div className={css('app')}>
       <Header />
       {children}
       <Footer />
